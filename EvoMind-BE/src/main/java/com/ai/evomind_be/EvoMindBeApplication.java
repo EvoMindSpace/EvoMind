ccdd7e94290d3da06db926eeb5018d5e8b680bb5
JohnDev-US <eworkann@gmail.com>
2025-02-04 23:17:10 +0700

    Add : ConversationController ,AiService,CreateConversationDetail,CreateConversation

--- EvoMind-BE/src/main/java/com/ai/evomind_be/EvoMindBeApplication.java
+++ EvoMind-BE/src/main/java/com/ai/evomind_be/EvoMindBeApplication.java
@@ -1,7 +1,6 @@
 package com.ai.evomind_be;
 
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.context.ApplicationContext;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
